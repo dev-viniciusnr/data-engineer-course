28a77ea61087fb1fdc434bdec58ef533f33e828b
Vinicius Nonato <v245394@dac.unicamp.br>
2025-08-04 20:42:31 -0300

    Getting started with SQLite and preparing a virtual environment

---
+++ 03_sqlite/01_virtual_enviroment.py
@@ -0,0 +1,10 @@
+##pip install virtualenv
+##ou pip3 install virtualenv
+
+#python -m venv  sgbds
+#sgbds\Scripts\activate
+
+#pip install pysqlite3
+#pip install --upgrade pysqlite3
+
+#installation of the library did not work, as sqlite3 is currently used
