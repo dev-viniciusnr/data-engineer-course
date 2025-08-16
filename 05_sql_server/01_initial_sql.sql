e7cd11c5d427078d473f8575bc05d289c1a755ad
Vinicius Nonato <v245394@dac.unicamp.br>
2025-08-16 15:53:16 -0300

    Starting with SQL server

---
+++ 05_sql_server/01_initial_sql.sql
@@ -0,0 +1,11 @@
+create table Teste (
+	Coluna1 nvarchar (255),
+	Coluna2 int,
+	Coluna3 money
+)
+
+select * from Teste
+
+insert into Teste (Coluna1, Coluna2, Coluna3) values ('Vinícius', 20, 300)
+
+truncate table Teste
