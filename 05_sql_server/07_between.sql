eb855675e09c62d04f2c73b1489f506e6339010f
Vinicius Nonato <v245394@dac.unicamp.br>
2025-08-18 20:02:47 -0300

    Searching with between

---
+++ 05_sql_server/07_between.sql
@@ -0,0 +1,21 @@
+select SalesOrderNumber, OrderQuantity, UnitPrice, SalesAmount
+from FactResellerSales
+where OrderQuantity between 5 and 10
+order by OrderQuantity
+
+select SalesOrderNumber, OrderQuantity, UnitPrice, SalesAmount
+from FactResellerSales
+where SalesAmount between 5000 and 10000
+order by 4
+
+select SalesOrderNumber, OrderQuantity, UnitPrice, SalesAmount
+from FactResellerSales
+where SalesAmount between 5000 and 10000
+and OrderQuantity between 5 and 15
+order by 4
+
+select SalesOrderNumber, OrderQuantity, UnitPrice, SalesAmount
+from FactResellerSales
+where SalesAmount between 5000 and 10000
+and OrderQuantity > 10
+order by 4
